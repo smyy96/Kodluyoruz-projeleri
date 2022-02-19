99fc1c393d6219fa21688af3855c2c2998d79618
smyy <smyy.cskn@hotmail.com>
2022-02-20 00:49:23 +0300

    2. Sorunun çözümü eklendi.

--- hafta1/Program.cs
+++ hafta1/Program.cs
@@ -6,11 +6,22 @@ namespace kodluyoruz_projeleri
     {
         static void Main(string[] args)
         {
-            Console.Write("Adınız : ");
+            // Alıştırma 1: Ad ve soyad bilgisini string değişenlere alarak ekranda yazdırma
+           
+           /* Console.Write("Adınız : ");
             string name=Console.ReadLine();
             Console.Write("Soyadınız : ");
             string surname=Console.ReadLine();
-            Console.WriteLine("Merhaba "+name+" "+surname);
+            Console.WriteLine("Merhaba "+name+" "+surname);*/
+
+
+            //Alıştırma 2: Kullanıcıdan 2 sayı isteyerek bu sayıların toplamını bulan program
+            Console.Write("1. Sayı : ");
+            int firstnum=int.Parse(Console.ReadLine());
+            Console.Write("2. Sayı : ");
+            int secondnum=int.Parse(Console.ReadLine());
+            Console.WriteLine(firstnum+" ve "+secondnum+" Sayının Toplamı : "+(firstnum+secondnum));     
+            
         }
     }
 }
