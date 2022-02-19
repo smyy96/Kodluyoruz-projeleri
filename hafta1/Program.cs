710fbbd277e064db5d7ade3dd50d5fb878247ac7
smyy <smyy.cskn@hotmail.com>
2022-02-20 01:02:06 +0300

    5. Soru yapıldı.

--- hafta1/Program.cs
+++ hafta1/Program.cs
@@ -7,7 +7,6 @@ namespace kodluyoruz_projeleri
         static void Main(string[] args)
         {
             // Alıştırma 1: Ad ve soyad bilgisini string değişenlere alarak ekranda yazdırma
-           
            /* Console.Write("Adınız : ");
             string name=Console.ReadLine();
             Console.Write("Soyadınız : ");
@@ -15,13 +14,17 @@ namespace kodluyoruz_projeleri
             Console.WriteLine("Merhaba "+name+" "+surname);*/
 
 
+
+
             //Alıştırma 2: Kullanıcıdan 2 sayı isteyerek bu sayıların toplamını bulan program
-            
             /*Console.Write("1. Sayı : ");
             int firstnum=int.Parse(Console.ReadLine());
             Console.Write("2. Sayı : ");
             int secondnum=int.Parse(Console.ReadLine());
             Console.WriteLine(firstnum+" ve "+secondnum+" Sayının Toplamı : "+(firstnum+secondnum));*/
+
+
+
 
             //Alıştırma 3: Dikdörtgenin alanını ve çevresini hesaplayarak ekranda gösteren program 
             /*Console.Write("Kısa Kenar : ");
@@ -32,14 +35,27 @@ namespace kodluyoruz_projeleri
             Console.WriteLine( "Çevre : "+(n+m)*2);*/
 
 
-            //Alıştırma 4: Girilen sayının tek mi çift mi olduğunu bulan program
 
-            Console.Write("Bir sayı giriniz : ");
+
+
+            //Alıştırma 4: Girilen sayının tek mi çift mi olduğunu bulan program
+            /*Console.Write("Bir sayı giriniz : ");
             int num=int.Parse(Console.ReadLine());
             if(num%2==0)
                 Console.Write(num+" çifttir.");
             else
-                Console.Write(num+" tektir.");
+                Console.Write(num+" tektir.");*/
+
+
+            //Alıştırma 5: Girilen sayının 0-100 arasında olup olmadığını kontrol eden program
+            Console.Write("Bir sayı giriniz : ");
+            int num=int.Parse(Console.ReadLine());
+            if(num>=0 && num<=100)
+                Console.Write(num+" sayısı 0-100 arasındadır.");
+            else
+                Console.Write(num+" sayısı 0-100 arasında değildir.");
+
+
         }
     }
 }
