5449f2a2895adedfa1f153632b99b4761e815b0c
smyy <smyy.cskn@hotmail.com>
2022-02-20 01:26:35 +0300

    7. soru yapıldı.

--- hafta1/Program.cs
+++ hafta1/Program.cs
@@ -7,53 +7,53 @@ namespace kodluyoruz_projeleri
         static void Main(string[] args)
         {
             // Alıştırma 1: Ad ve soyad bilgisini string değişenlere alarak ekranda yazdırma
-           /* Console.Write("Adınız : ");
+            Console.Write("Adınız : ");
             string name=Console.ReadLine();
             Console.Write("Soyadınız : ");
             string surname=Console.ReadLine();
-            Console.WriteLine("Merhaba "+name+" "+surname);*/
+            Console.WriteLine("Merhaba "+name+" "+surname);
 
 
 
 
             //Alıştırma 2: Kullanıcıdan 2 sayı isteyerek bu sayıların toplamını bulan program
-            /*Console.Write("1. Sayı : ");
+            Console.Write("1. Sayı : ");
             int firstnum=int.Parse(Console.ReadLine());
             Console.Write("2. Sayı : ");
             int secondnum=int.Parse(Console.ReadLine());
-            Console.WriteLine(firstnum+" ve "+secondnum+" Sayının Toplamı : "+(firstnum+secondnum));*/
+            Console.WriteLine(firstnum+" ve "+secondnum+" Sayının Toplamı : "+(firstnum+secondnum));
 
 
 
 
             //Alıştırma 3: Dikdörtgenin alanını ve çevresini hesaplayarak ekranda gösteren program 
-            /*Console.Write("Kısa Kenar : ");
+            Console.Write("Kısa Kenar : ");
             int n=int.Parse(Console.ReadLine());
             Console.Write("Uzun Kenar : ");
             int m=int.Parse(Console.ReadLine());
             Console.WriteLine( "Alan : "+(n*m));
-            Console.WriteLine( "Çevre : "+(n+m)*2);*/
+            Console.WriteLine( "Çevre : "+(n+m)*2);
 
 
 
 
 
             //Alıştırma 4: Girilen sayının tek mi çift mi olduğunu bulan program
-            /*Console.Write("Bir sayı giriniz : ");
-            int num=int.Parse(Console.ReadLine());
-            if(num%2==0)
-                Console.Write(num+" çifttir.");
+            Console.Write("Bir sayı giriniz : ");
+            int numb=int.Parse(Console.ReadLine());
+            if(numb%2==0)
+                Console.Write(numb+" çifttir.");
             else
-                Console.Write(num+" tektir.");*/
+                Console.Write(numb+" tektir.");
 
 
             //Alıştırma 5: Girilen sayının 0-100 arasında olup olmadığını kontrol eden program
-            /*Console.Write("Bir sayı giriniz : ");
+            Console.Write("Bir sayı giriniz : ");
             int num=int.Parse(Console.ReadLine());
             if(num>=0 && num<=100)
                 Console.Write(num+" sayısı 0-100 arasındadır.");
             else
-                Console.Write(num+" sayısı 0-100 arasında değildir.");*/
+                Console.Write(num+" sayısı 0-100 arasında değildir.");
 
 
             //6: Vücut Kitle İndeksi hesaplayan program
@@ -63,6 +63,11 @@ namespace kodluyoruz_projeleri
             float height=float.Parse(Console.ReadLine());
             Console.WriteLine( "Vücut kitle indeksi : "+(weight/(height*height)));
 
+
+            //Alıştırma 7: Cümledeki harf sayısını bulan program
+            Console.Write("Cümle giriniz : ");
+            string word=Console.ReadLine();
+            Console.Write(word.Trim().Replace(" ", string.Empty).Length);
 
         }
     }
