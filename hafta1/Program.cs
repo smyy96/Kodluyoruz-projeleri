7964a38543a887667e3bec1add2dbf0413adcbc4
smyy <smyy.cskn@hotmail.com>
2022-02-20 00:43:01 +0300

    first commit

---
+++ hafta1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace kodluyoruz_projeleri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Adınız : ");
+            string name=Console.ReadLine();
+            Console.Write("Soyadınız : ");
+            string surname=Console.ReadLine();
+            Console.WriteLine("Merhaba "+name+" "+surname);
+        }
+    }
+}
