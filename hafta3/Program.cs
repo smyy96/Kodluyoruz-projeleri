2483ea1b63fb56532abf194adc911109c0907309
smyy <smyy.cskn@hotmail.com>
2022-03-06 18:56:26 +0300

    hafta 3

--- hafta3/Program.cs
+++ hafta3/Program.cs
@@ -5,7 +5,7 @@ namespace hafta3
     class Program
     {
         public static void soru1(){
-           //int count=0;
+           
            string[] words=Console.ReadLine().Split(' ');
            Console.WriteLine(words.Length);
         }
