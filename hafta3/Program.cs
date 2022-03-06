8c64a65f1819a3f7b4b2264edfb65d0dbcaf2848
smyy <smyy.cskn@hotmail.com>
2022-03-06 18:55:13 +0300

    Hafta 3 first commit

---
+++ hafta3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace hafta3
+{
+    class Program
+    {
+        public static void soru1(){
+           //int count=0;
+           string[] words=Console.ReadLine().Split(' ');
+           Console.WriteLine(words.Length);
+        }
+        static void Main(string[] args)
+        {
+            soru1();
+        }
+    }
+}
