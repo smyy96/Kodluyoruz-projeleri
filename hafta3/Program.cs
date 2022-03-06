47533572cc68a65af0e1d26ca11bf558a1f123ed
smyy <smyy.cskn@hotmail.com>
2022-03-06 19:28:38 +0300

    2.soru

--- hafta3/Program.cs
+++ hafta3/Program.cs
@@ -4,14 +4,33 @@ namespace hafta3
 {
     class Program
     {
-        public static void soru1(){
+        public static void soru1(){ //Klavyeden girilen metnin kaç kelimeden oluştuğunu bulan program.
            
+           Console.Write("Cümle giriniz: ");
            string[] words=Console.ReadLine().Split(' ');
-           Console.WriteLine(words.Length);
+           Console.WriteLine("Kelime sayısı: "+words.Length);
+        }
+
+
+        public static void soru2() //Verilen bir karakter dizininin substring() metodunu kullanarak string içerisinde arama yapan ve kaç defa geçtiğini bulan program.
+        {
+            int count=0;
+             Console.Write("Cümle giriniz: ");
+             string[] words=Console.ReadLine().Split(' ');
+             Console.Write("Aranacak kelimeyi giriniz: ");
+             string word=Console.ReadLine();
+             for(int i=0;i<words.Length;i++){
+                 if(words[i].Substring(0)==word){
+                     count++;   
+                 }
+             }
+             Console.Write(word+" cümle içindeki sayısı "+count+"dir.");
+
         }
         static void Main(string[] args)
         {
-            soru1();
+            //soru1();
+            soru2();
         }
     }
 }
